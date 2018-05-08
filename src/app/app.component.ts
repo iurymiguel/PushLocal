0cb1b06ce515028b6607b46e40d4278a88360953
Iury da Rocha Miguel <iuryrocha@inatel.br>
2018-05-08 10:16:05 -0300

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,59 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private push: Push) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.initPushNotification();
     });
   }
+
+  initPushNotification() {
+    const options: PushOptions = {
+      android: {
+        senderID: '991987094804',
+      },
+      ios: {
+        alert: 'true',
+        badge: true,
+        sound: 'false'
+      },
+      windows: {}
+    }
+
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on('registration').subscribe((data: any) => {
+      console.log('device token '+data.registrationId);
+      console.log('registrationType '+data.registrationType);
+    });
+
+    pushObject.on('notification').subscribe((data: any) => {
+      console.log('titleNotification '+data.title);
+      console.log('AdditionalData '+data.additionalData.customData);
+
+
+      if(data.additionalData.foreground){
+      
+      }
+
+
+    });
+
+   
+
+  }
+
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [LocalNotifications]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private localNotifications: LocalNotifications) {
 
+  }
+
+  notify(){
+    this.localNotifications.schedule({
+      id: 1,
+      title: 'Local ILocalNotification Example',
+      text: 'Single ILocalNotification',
+      priority: 2,
+      data: { secret: 'key' }
+    });
+
+    this.localNotifications.on('click').subscribe((notification) => {
+        console.log('clicked');
+        console.log(notification);
+        
+    });
+  
   }
 
 }
